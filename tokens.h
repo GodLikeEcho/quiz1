f0c5a3e2d6267e965f21b70a794ab7c47470b10b
godlikeecho <victosme@gmail.com>
2016-01-12 19:56:18 +0000

    Test files

---
+++ tokens.h
@@ -0,0 +1,24 @@
+#pragma once
+
+//**************************************
+// tokens.h
+//
+// Defines the values for the tokens returned by yylex
+//
+// Author: Phil Howard 
+// phil.howard@oit.edu
+//
+// Date: Dec 30, 2015
+//
+
+#define STUFF       1000
+#define OTHER       1001
+#define BLOB1       1002
+#define BLOB2       1003
+#define JUNK        2000
+#define LBRACKET    '['
+#define STAR        '*'
+
+
+
+
